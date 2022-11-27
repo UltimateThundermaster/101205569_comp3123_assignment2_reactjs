be38f7eedbb2e6533af76e25f125f0e3132d2179
Artem Elagin <artem.elagin@georgebrown.ca>
2022-11-26 23:36:19 -0500

    adding production code

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const mongoose = require("mongoose")
 const userRouter = require("./routes/user")
 const employeeRouter = require("./routes/employee")
 const auth = require("./middleware/auth")
+const path = require('path')
 const app = express()
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build')) 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  }) 
+}
 
 const mongoAtlasUri = "mongodb+srv://ThunderMaster:vz_G3URks-S.2km@cluster0.phfivi1.mongodb.net/comp3123_assigment1?retryWrites=true&w=majority"
 
